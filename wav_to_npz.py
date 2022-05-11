e93edb530f15f4681abf9f87352704ef9ed08cc6
Drew Wadsworth <drew.wadsworth@gmail.com>
2022-05-11 16:14:46 -0700

    Add npz to wav functionality to wav_to_npz.py

--- wav_to_npz.py
+++ wav_to_npz.py
@@ -1,9 +1,11 @@
 
 import numpy as np
-from scipy.io.wavfile import read
+from scipy.io.wavfile import read, write
 
-wav_filepath = '../../../Downloads/CS230Piano.wav'  # Need to change this
+wav_in_filepath = '../../../Downloads/CS230Piano.wav'  # Need to change this
 npz_filepath = 'CS230Piano.npz'
+wav_out_filepath = 'CS230PianoTransformed.wav'
+sample_rate_out = 0 # Need to change this
 
 def wav_to_arr(filepath):
   a = read(filepath)
@@ -16,5 +18,14 @@ def wav_to_arr(filepath):
 def arr_to_npz(filepath, arr):
   np.savez(filepath, arr)
 
-arr = wav_to_arr(wav_filepath)
-arr_to_npz(npz_filepath, arr)
+def wav_to_npz():
+  arr = wav_to_arr(wav_in_filepath)
+  arr_to_npz(npz_filepath, arr)
+
+def npz_to_wav():
+  arr = np.load(npz_filepath)
+  arr = arr['arr_0']
+  write(wav_out_filepath, sample_rate_out, arr)
+
+# wav_to_npz()
+# npz_to_wav()
